8cc20fd4209d0ba2c8338b4f2f5fc3815100ddde
Pedro Castanheira <josepedrocastanheira@gmail.com>
2016-11-16 15:26:54 +0000

    Revisão do código e correção de bugs

--- banco.rb
+++ banco.rb
@@ -1,7 +1,7 @@
-saldo_inicial = 1000.to_i
+saldo_inicial = ["Saldo Inicial",1000.to_i]
 movimentos = []
 movimentos.push(saldo_inicial)
-acumulado = saldo_inicial
+acumulado = saldo_inicial[1]
 op_adicional = "1"
 op_adicional_arr = ["1", "s", "S", "sim", "Sim"]
 
@@ -24,21 +24,56 @@ opcao_consulta = ["c", "C", "consulta" , "Consulta", "saldo", "Saldo"]
 if opcao_deposito.include? tipo_operacao
   print "Quantia a DEPOSITAR => "
   deposito = gets.chomp
-  movimentos.push(deposito.to_i)
-  puts "#{movimentos}"
+  movimentos.push(["Deposito",deposito.to_i])
   acumulado = acumulado + deposito.to_i
-  puts "#{acumulado}"
   puts "Pretende realizar mais alguma operação?"
   print "1-SIM ou 2-NÃO => "
   op_adicional = gets.chomp
 elsif opcao_levantamento.include? tipo_operacao
   print "Quantia a LEVANTAR => "
   levantamento = gets.chomp
+    while levantamento.to_i % 5 != 0
+    puts "O levantamento tem de ser múltiplo de 5 €"
+    print "Quantia a LEVANTAR => "
+    levantamento = gets.chomp    
+    end
+
+  # Algoritmo de entrega de notas
+  lev_aux = levantamento.to_i
+  tipo_notas = [5,10,20,50,100]
+  notas_entregar = []
+  
+  if lev_aux >= 100 then max_nota = tipo_notas[4]; end
+  if lev_aux >= 80 && lev_aux < 100 then max_nota = tipo_notas[3]; end
+  if lev_aux >= 40 && lev_aux < 80 then max_nota = tipo_notas[2]; end
+  if lev_aux > 10 && lev_aux < 40 then max_nota = tipo_notas[1]; end
+  if lev_aux <= 10 then max_nota = tipo_notas[0]; end
+
+  i = tipo_notas.index(max_nota)
+
+  while i >= 0
+  if i > 0
+    num_notas = (lev_aux/tipo_notas[i]-1).to_i
+    lev_aux = lev_aux.to_i - (tipo_notas[i].to_i * num_notas)
+    notas_entregar.push(["#{tipo_notas[i]}",num_notas])
+    i -= 1
+  else
+    num_notas = (lev_aux/tipo_notas[i]).to_i
+    lev_aux = lev_aux.to_i - (tipo_notas[i].to_i * num_notas)
+    notas_entregar.push(["#{tipo_notas[i]}",num_notas])
+    i -= 1
+  end
+  end
+  
+  puts
+  puts "Notas a Entregar:" 
+  notas_entregar.each {|x| puts "#{x[0]} €: #{x[1]}"}
+  puts
+  # Fim do algoritmo de entrega de notas
+
   levantamento = levantamento.to_i * -1
-  movimentos.push(levantamento)
-  puts "#{movimentos}"
+  movimentos.push(["Levantamento",levantamento])
   acumulado = acumulado + levantamento.to_i
-  puts "#{acumulado}"
   puts "Pretende realizar mais alguma operação?"
   print "1-SIM ou 2-NÃO => "
   op_adicional = gets.chomp
@@ -46,16 +81,16 @@ elsif opcao_transferencia.include? tipo_operacao
   print "Quantia a TRANSFERIR => "
   transferencia = gets.chomp
   transferencia = transferencia.to_i * -1
-  movimentos.push(transferencia)
-  puts "#{movimentos}"
+  movimentos.push(["Transferencia",transferencia])
   acumulado = acumulado + transferencia.to_i
-  puts "#{acumulado}"
   puts "Pretende realizar mais alguma operação?"
   print "1-SIM ou 2-NÃO => "
   op_adicional = gets.chomp
 elsif opcao_consulta.include? tipo_operacao
-  lineWidth = 20
-  movimentos.each {|x| puts "Movimento n.º #{movimentos.index(x)}:".ljust(lineWidth) + "#{x} €".rjust(lineWidth)}
+  lineWidth = 35
+  movimentos.each do |x|
+    puts "Tipo de Movimento => #{x[0]}:".ljust(lineWidth) + "#{x[1]} €".rjust(lineWidth)
+  end
   puts "O seu saldo é de:".ljust(lineWidth) + "#{acumulado} €".rjust(lineWidth)
   puts
   puts "Pretende realizar mais alguma operação?"
